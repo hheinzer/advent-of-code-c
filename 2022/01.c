f250ea572a2937f4398dce7bfef5d5b2c9f7860c
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2022-12-31 18:42:38 +0100

    cleanup

--- 2022/01.c
+++ 2022/01.c
@@ -1,10 +1,15 @@
 /*
- * --- Day 1: Calorie Counting ---
+ * Day 1: Calorie Counting
+ * (https://adventofcode.com/2022/day/1)
  *
- *  Part 1:
- *  -
+ * Part 1:
+ * - add up calories that each elf carries
+ * - sort calories in descending order
+ * - the elf that carries the most calories is the first one
+ *
+ * Part 2:
+ * - the 3 elves with the most calories are the first 3 in the sorted list
  */
-
 #include "aoc.c"
 
 int main(void)
@@ -14,6 +19,30 @@ int main(void)
     size_t n_lines = 0;
     lines_read("01.txt", &line, &n_lines);
 
+    // add up calories of every elf, append to cals
+    long *cals = calloc(1, sizeof(*cals));
+    size_t n_cals = 1;
+    for (size_t i = 0; i < n_lines; ++i) {
+        if (strlen(line[i]) > 0) {
+            long cal = 0;
+            sscanf(line[i], "%ld", &cal);
+            cals[n_cals - 1] += cal;
+        } else {
+            cals = reallocarray(cals, ++n_cals, sizeof(*cals));
+            cals[n_cals - 1] = 0;
+        }
+    }
+
+    // sort calories
+    qsort(cals, n_cals, sizeof(*cals), cmp_long_dsc);
+
+    // part 1
+    printf("%ld\n", cals[0]);
+
+    // part 2
+    printf("%ld\n", SUM(cals, 3));
+
     // cleanup
     lines_free(line, n_lines);
+    free(cals);
 }
--- aoc.c
+++ aoc.c
@@ -1,22 +1,39 @@
 /*
  * helper functions for advent of code puzzles
  */
-
 #include <assert.h>
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// size of a static array
+#define LEN(a) (sizeof(a) / sizeof(*a))
+
+// type generic min and max functions
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define LEN(a) (sizeof(a) / sizeof(*a))
+
+// cast a flat array to a multidimensional one
 #define TENSOR(t, a) (__typeof__(t))a
 
-/*
- * read all lines in file "fname" into lines, replace '\n' with '\0'
- */
-static void lines_read(const char *fname, char ***lines, size_t *n_lines)
+// type generic array summation function
+#define SUM(a, n) \
+    ({ __typeof__(*a) s = 0; for (size_t i = 0; i < n; ++i) s += a[i]; s; })
+
+// type generic array multiplication function
+#define PROD(a, n) \
+    ({ __typeof__(*a) p = 1; for (size_t i = 0; i < n; ++i) p *= a[i]; p; })
+
+// simple comparison function for ascending and descending order
+#define CMP(T)                                                                    \
+    int cmp_##T##_asc(const void *a, const void *b) { return *(T *)a - *(T *)b; } \
+    int cmp_##T##_dsc(const void *a, const void *b) { return *(T *)b - *(T *)a; }
+CMP(long);
+CMP(double);
+
+// read all lines in file "fname" into lines, replace '\n' with '\0'
+void lines_read(const char *fname, char ***lines, size_t *n_lines)
 {
     // open file
     FILE *file = fopen(fname, "r");
@@ -51,10 +68,8 @@ static void lines_read(const char *fname, char ***lines, size_t *n_lines)
     fclose(file);
 }
 
-/*
- * free all lines
- */
-static void lines_free(char **line, size_t n_lines)
+// free all lines
+void lines_free(char **line, size_t n_lines)
 {
     for (size_t i = 0; i < n_lines; ++i) {
         free(line[i]);
