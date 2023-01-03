f6b7457ede3c6eb6cba48d7e372eec8722de8875
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2023-01-03 15:09:03 +0100

    added line_index(), added general purpose (doubley linked) list

--- aoc.c
+++ aoc.c
@@ -2,6 +2,7 @@
  * helper functions for advent of code puzzles
  */
 #include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -98,4 +99,192 @@ void lines_free(char **line, size_t n_lines)
         free(line[i]);
     }
     free(line);
+}
+
+// find index of first line that matches
+size_t line_index(char **line, size_t n_lines, const char *find)
+{
+    for (size_t i = 0; i < n_lines; ++i) {
+        if (!strcmp(line[i], find)) {
+            return i;
+        }
+    }
+    assert(!"No matching line found.");
+}
+
+// general purpose (doubly linked) list data structure
+typedef struct List List;
+typedef struct ListElement ListElement;
+struct List {
+    size_t data_size;
+    size_t nelem;
+    ListElement *head;
+    ListElement *tail;
+};
+struct ListElement {
+    void *data;
+    ListElement *next;
+    ListElement *prev;
+};
+
+// create list
+List list_create(size_t data_size)
+{
+    return (List) { .data_size = data_size };
+}
+
+// free list
+void list_free(List *list)
+{
+    ListElement *elem = list->head;
+    for (size_t i = 0; i < list->nelem; ++i) {
+        ListElement *next = elem->next;
+        free(elem->data);
+        free(elem);
+        elem = next;
+    }
+}
+
+// insert element onto list at position ielem
+void list_insert(List *list, size_t ielem, void *data)
+{
+    assert(ielem <= list->nelem && "Illegal index encountered.");
+
+    // create new element
+    ListElement *elem = calloc(1, sizeof(*elem));
+    elem->data = data;
+
+    if (list->nelem == 0) {
+        // empty list: new element is head and tail of list
+        list->head = elem;
+        list->tail = elem;
+
+    } else if (ielem == 0) {
+        // new element is new head of list
+        elem->next = list->head;
+        list->head->prev = elem;
+        list->head = elem;
+
+    } else if (ielem == list->nelem) {
+        // new element is new tail of list
+        elem->prev = list->tail;
+        list->tail->next = elem;
+        list->tail = elem;
+
+    } else {
+        // insert element into list, take shortest path
+        ListElement *next = 0;
+        if (ielem <= list->nelem - 1 - ielem) {
+            next = list->head;
+            for (size_t i = 0; i < ielem; ++i) {
+                next = next->next;
+            }
+
+        } else {
+            next = list->tail;
+            for (size_t i = 0; i < list->nelem - 1 - ielem; ++i) {
+                next = next->prev;
+            }
+        }
+        elem->next = next;
+        elem->prev = next->prev;
+        elem->next->prev = elem;
+        elem->prev->next = elem;
+    }
+
+    // increase element counter
+    ++list->nelem;
+}
+void list_push_front(List *list, void *data)
+{
+    list_insert(list, 0, data);
+}
+void list_push_back(List *list, void *data)
+{
+    list_insert(list, list->nelem, data);
+}
+void list_push_front_copy(List *list, void *data)
+{
+    void *copy = malloc(list->data_size);
+    memcpy(copy, data, list->data_size);
+    list_push_front(list, copy);
+}
+void list_push_back_copy(List *list, void *data)
+{
+    void *copy = malloc(list->data_size);
+    memcpy(copy, data, list->data_size);
+    list_push_back(list, copy);
+}
+
+// remove element from list at position ielem, return its data pointer
+void *list_remove(List *list, size_t ielem)
+{
+    assert(list->nelem > 0 && "Cannot pop element from empty list.");
+    assert(ielem < list->nelem && "Illegal index encountered.");
+
+    ListElement *elem = 0;
+    if (list->nelem == 1) {
+        // element is last element in list
+        elem = list->head;
+        list->head = 0;
+        list->tail = 0;
+
+    } else if (ielem == 0) {
+        // element is head of list
+        elem = list->head;
+        list->head = list->head->next;
+        list->head->prev = 0;
+
+    } else if (ielem == list->nelem - 1) {
+        // element is tail of list
+        elem = list->tail;
+        list->tail = list->tail->prev;
+        list->tail->next = 0;
+
+    } else {
+        // find element in list, take shortest path
+        if (ielem <= list->nelem - 1 - ielem) {
+            elem = list->head;
+            for (size_t i = 0; i < ielem; ++i) {
+                elem = elem->next;
+            }
+
+        } else {
+            elem = list->tail;
+            for (size_t i = 0; i < list->nelem - 1 - ielem; ++i) {
+                elem = elem->prev;
+            }
+        }
+        elem->next->prev = elem->prev;
+        elem->prev->next = elem->next;
+    }
+
+    // get data pointer
+    void *data = elem->data;
+
+    // remove element
+    free(elem);
+
+    // decrease element counter
+    --list->nelem;
+
+    return data;
+}
+void *list_pop_front(List *list)
+{
+    return list_remove(list, 0);
+}
+void *list_pop_back(List *list)
+{
+    return list_remove(list, list->nelem - 1);
+}
+
+// copy list
+List list_copy(List *list)
+{
+    List copy = list_create(list->data_size);
+    for (ListElement *elem = list->head; elem; elem = elem->next) {
+        list_push_back_copy(&copy, elem->data);
+    }
+    return copy;
 }
