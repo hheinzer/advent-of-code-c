1bea1966621f04ac62689c534ced61c91ca16ca8
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2023-01-04 12:45:05 +0100

    cleanup

--- 2022/01.c
+++ 2022/01.c
@@ -13,7 +13,6 @@
 #include "aoc.c"
 
 CMP(long)
-SUM(long)
 
 int main(void)
 {
@@ -42,7 +41,7 @@ int main(void)
     printf("%ld\n", cals[0]);
 
     // part 2
-    printf("%ld\n", sum_long(cals, 3));
+    printf("%ld\n", cals[0] + cals[1] + cals[2]);
 
     // cleanup
     lines_free(line, n_lines);
