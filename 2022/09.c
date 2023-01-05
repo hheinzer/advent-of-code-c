bb9d353038f3fc728616a8ac9d99162e3a1de486
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2023-01-05 17:11:16 +0100

    added 09

---
+++ 2022/09.c
@@ -0,0 +1,89 @@
+/*
+ * Day 9: Rope Bridge
+ * (https://adventofcode.com/2022/day/9)
+ *
+ * Part 1:
+ *
+ * Part 2:
+ */
+#include "aoc.c"
+
+typedef struct Position {
+    long x;
+    long y;
+} Position;
+
+void position_move(Position *pos, char dir)
+{
+    switch (dir) {
+    case 'L':
+        --pos->x;
+        break;
+    case 'R':
+        ++pos->x;
+        break;
+    case 'U':
+        ++pos->y;
+        break;
+    case 'D':
+        --pos->y;
+        break;
+    default:
+        assert(!"Illegal direction encountered.");
+    }
+}
+
+void position_follow(Position *tail, const Position *head)
+{
+    const long dx = head->x - tail->x;
+    const long dy = head->y - tail->y;
+    if ((labs(dx) == 2) || labs(dy) == 2) {
+        tail->x += SIGN(dx) * CEIL(labs(dx), 2);
+        tail->y += SIGN(dy) * CEIL(labs(dy), 2);
+    }
+}
+
+int main(void)
+{
+    // read input
+    char **line = 0;
+    const size_t n_lines = lines_read(&line, "09.txt");
+
+    // simulate rope with 2 and 10 knots
+    const size_t nr[] = { 2, 10 };
+    for (size_t r = 0; r < LEN(nr); ++r) {
+        // initialize rope
+        Position rope[nr[r]];
+        memset(rope, 0, sizeof(rope));
+
+        // create hash table for tail positions (used as set: data=0)
+        Htable *tail_pos = htable_create(0, 20000);
+
+        // run simulation
+        for (size_t i = 0; i < n_lines; ++i) {
+            char dir = 0;
+            size_t n = 0;
+            sscanf(line[i], "%c %zu", &dir, &n);
+            for (size_t j = 0; j < n; ++j) {
+                position_move(&rope[0], dir);
+                for (size_t k = 1; k < nr[r]; ++k) {
+                    position_follow(&rope[k], &rope[k - 1]);
+                }
+
+                // insert tail position into hash table
+                char key[22] = "";
+                sprintf(key, "%+09ld %+09ld", rope[nr[r] - 1].x, rope[nr[r] - 1].y);
+                htable_insert(tail_pos, key, 0);
+            }
+        }
+
+        // print result
+        printf("%zu\n", tail_pos->nelem);
+
+        // cleanup
+        htable_free(tail_pos);
+    }
+
+    // cleanup
+    lines_free(line, n_lines);
+}
