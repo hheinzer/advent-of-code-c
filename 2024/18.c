567491683db9f1b21224397faa16f4e209a104fa
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-01-16 16:54:07 +0100

    2024/18

---
+++ 2024/18.c
@@ -0,0 +1,64 @@
+#include "aoc.h"
+
+void parse(List *byte, const char *fname);
+long min_distance(const Grid *grid, Arena arena);
+
+int main(void) {
+    Arena arena = arena_create(1 << 20);
+
+    List byte = list_create(&arena, sizeof(Vec2), 0);
+    parse(&byte, "2024/input/18.txt");
+
+    long size = 71;
+    Grid grid = grid_create(size, size, '.', &arena);
+
+    long count = 1024;
+    list_for_each(item, &byte) {
+        Vec2 *pos = item->data;
+        grid_set(&grid, pos->r, pos->c, '#');
+        if (--count == 0) {
+            printf("%ld\n", min_distance(&grid, arena));
+        }
+        if (count < 0 && min_distance(&grid, arena) < 0) {
+            printf("%ld,%ld\n", pos->c, pos->r);
+            break;
+        }
+    }
+
+    arena_destroy(&arena);
+}
+
+void parse(List *byte, const char *fname) {
+    FILE *file = fopen(fname, "r");
+    assert(file);
+    long c, r;
+    while (fscanf(file, "%ld,%ld\n", &c, &r) == 2) {
+        list_append(byte, &(Vec2){r, c});
+    }
+    fclose(file);
+}
+
+long min_distance(const Grid *grid, Arena arena) {
+    Vec2 start = {0, 0};
+    Vec2 end = {grid->rows - 1, grid->cols - 1};
+    long(*dist)[grid->cols] = calloc(&arena, dist, grid->rows);
+    List queue = list_create(&arena, sizeof(Vec2), 0);
+    list_append(&queue, &start);
+    while (queue.length) {
+        Vec2 *cur = list_pop(&queue, 0);
+        if (cur->r == end.r && cur->c == end.c) {
+            return dist[cur->r][cur->c];
+        }
+        array_for_each(Vec2, dir, {-1, 0}, {+1, 0}, {0, -1}, {0, +1}) {
+            Vec2 nxt = {cur->r + dir->r, cur->c + dir->c};
+            if (grid_get(grid, nxt.r, nxt.c) != '.') {
+                continue;
+            }
+            if (!dist[nxt.r][nxt.c]) {
+                dist[nxt.r][nxt.c] = dist[cur->r][cur->c] + 1;
+                list_append(&queue, &nxt);
+            }
+        }
+    }
+    return -1;
+}
