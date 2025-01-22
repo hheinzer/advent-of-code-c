edf94d315a155b6805e0e2498bd5d7d87bfc97f2
Heinz Heinrich Heinzer <heinrich.heinzer@gmail.com>
2025-01-22 11:04:28 +0100

    increase performace by doing less work

--- 2024/23.c
+++ 2024/23.c
@@ -1,41 +1,34 @@
 #include "aoc.h"
 
 void parse(Dict *network, const char *fname, Arena *arena);
-void search(const Dict *network, const char *name, Set *cycle, Set *set, Arena arena);
-int check(const char *cycle);
-char *longest(const Set *cycle);
+long search(const Dict *network, char *name, List *current, char *longest, Arena arena);
 
 int main(void) {
-    Arena arena = arena_create(23 * mega_byte);
-    Arena scratch = arena_scratch_create(&arena, mega_byte);
+    Arena arena = arena_create(mega_byte);
 
-    Dict network = dict_create(&arena, sizeof(Set));
+    Dict network = dict_create(&arena, sizeof(List));
     parse(&network, "2024/input/23.txt", &arena);
 
-    Set cycle = set_create(&arena);
-    dict_for_each(item, &network) {
-        search(&network, item->key.data, &cycle, nullptr, scratch);
-    }
-
     long part1 = 0;
-    set_for_each(item, &cycle) {
-        part1 += check(item->key.data);
+    char part2[256] = "";
+    dict_for_each(item, &network) {
+        part1 += search(&network, item->key.data, nullptr, part2, arena);
     }
-    printf("%ld\n", part1);
-    printf("%s\n", longest(&cycle));
+    printf("%ld\n", part1 / 3);
+    printf("%s\n", part2);
 
     arena_destroy(&arena);
 }
 
 void add_edge(Dict *network, const char *name, const char *edge, Arena *arena) {
-    Set *connection = dict_find(network, name, 0);
+    List *connection = dict_find(network, name, 0);
     if (!connection) {
-        Set set = set_create(arena);
-        set_insert(&set, edge, 0);
-        dict_insert(network, name, 0, &set);
+        List list = list_create(arena, 0, cmp_string);
+        list_append(&list, strdup(arena, edge));
+        dict_insert(network, name, 0, &list);
     }
     else {
-        set_insert(connection, edge, 0);
+        list_append(connection, strdup(arena, edge));
     }
 }
 
@@ -50,70 +43,53 @@ void parse(Dict *network, const char *fname, Arena *arena) {
     fclose(file);
 }
 
-int keycmp(const void *a, const void *b) {
-    return memcmp(a, b, 2);
-}
-
-int set_insert_sorted(Set *cycle, const Set *set, Arena arena) {
-    char(*key)[3] = calloc(&arena, key, set->length);
-    long i = 0;
-    set_for_each(item, set) {
-        strncpy(key[i], item->key.data, 2);
-        key[i++][2] = ',';
+bool check1(const List *clique) {
+    if (clique->length != 3) {
+        return false;
     }
-    qsort(key, set->length, sizeof(*key), keycmp);
-    key[set->length - 1][2] = 0;
-    return set_insert(cycle, key, 0);
-}
-
-bool set_is_subset(const Set *self, const Set *other) {
-    set_for_each(item, self) {
-        if (!set_find(other, item->key.data, item->key.size)) {
-            return false;
+    list_for_each(item, clique) {
+        char *name = item->data;
+        if (name[0] == 't') {
+            return true;
         }
     }
-    return true;
+    return false;
 }
 
-void search(const Dict *network, const char *name, Set *cycle, Set *set, Arena arena) {
-    if (!set) {
-        set = (Set[]){set_create(&arena)};
-        set_insert(set, name, 0);
-    }
-    if (set->length >= 3 && set_insert_sorted(cycle, set, arena)) {
-        return;
-    }
-    Set *connection = dict_find(network, name, 0);
-    set_for_each(item, connection) {
-        char *neighbor = item->key.data;
-        if (set_find(set, neighbor, 0)) {
-            continue;
+void check2(List *clique, char *longest) {
+    if (3 * clique->length - 1 > (long)strlen(longest)) {
+        longest[0] = 0;
+        list_sort(clique, nullptr);
+        list_for_each(item, clique) {
+            strcat(longest, item->data);
+            strcat(longest, item->next ? "," : "");
         }
-        if (!set_is_subset(set, dict_find(network, neighbor, 0))) {
-            continue;
-        }
-        Set clone = set_clone(set, &arena);
-        set_insert(&clone, neighbor, 0);
-        search(network, neighbor, cycle, &clone, arena);
     }
 }
 
-int check(const char *cycle) {
-    long length = 0;
-    long t_count = 0;
-    for (const char *chr = cycle; *chr; chr += 3) {
-        length += 1;
-        t_count += *chr == 't';
+long search(const Dict *network, char *name, List *current, char *longest, Arena arena) {
+    long count = 0;
+    if (!current) {
+        List clique = list_create(&arena, 0, cmp_string);
+        list_append(&clique, name);
+        current = (List[]){clique, *(List *)dict_find(network, name, 0)};
     }
-    return length == 3 && t_count > 0;
-}
-
-char *longest(const Set *cycle) {
-    char *key = "";
-    set_for_each(item, cycle) {
-        if (strlen(item->key.data) > strlen(key)) {
-            key = item->key.data;
+    List *clique = &current[0];
+    List *candidates = &current[1];
+    count += check1(clique);
+    check2(clique, longest);
+    list_for_each(item, candidates) {
+        char *candidate = item->data;
+        List *connection = dict_find(network, candidate, 0);
+        List next_clique = list_clone(clique, &arena);
+        list_append(&next_clique, candidate);
+        List next_candidates = list_create(&arena, 0, nullptr);
+        for (ListItem *next = item->next; next; next = next->next) {
+            if (list_find(connection, next->data)) {
+                list_append(&next_candidates, next->data);
+            }
         }
+        count += search(network, candidate, (List[]){next_clique, next_candidates}, longest, arena);
     }
-    return key;
+    return count;
 }
